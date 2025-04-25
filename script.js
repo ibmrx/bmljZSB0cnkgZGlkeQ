90a2debf3eb3696b8ad4a39d86e67908d5458603
ibmrx <7ibmrx@gmail.com>
2025-04-25 20:46:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ function showModule(module) {
             moduleTitle = 'Programmation';
             moduleContent = `
                 <p>Les Bases Algorithmes de la Programmation en C. <a href="https://drive.google.com/file/d/1mOWFU3CUJrnzMs46Lrz9ha-R55EkZJ6v/view?usp=sharing" target="_blank">Télécharger le PDF</a></p>
+                <p>Les Sous Programmes ~ Mme. Amrane Cylia. <a href="https://drive.google.com/file/d/1E6sN3ZsFePPLWLAqWoOTtlbAjXMzEH-f/view?usp=drive_link" target="_blank">Télécharger le PDF</a></p>
             `;
             break;
         case 'ang':
